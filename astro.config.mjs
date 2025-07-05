e425e975efdd65bda97d5631497909348ccbb040
LetMeFixThis4U <charlesroyluc@gmail.com>
2025-07-05 17:58:01 +0200

    <Update> : Github Action configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://LetMeFixThis4U.github.io",
+  base: "portefolio",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
